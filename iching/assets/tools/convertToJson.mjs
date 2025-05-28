ef895aec9d8206fb17b4c4ae6536f5601d8780d7
Andrew Barker <103617658+Barkwanderer@users.noreply.github.com>
2025-05-28 13:51:01 -0400

    js object conversion script stored here in case need to use again

---
+++ iching/assets/tools/convertToJson.mjs
@@ -0,0 +1,15 @@
+// convertToJson.mjs for use in a node environment, easiest way to make  valid JSON from JS object
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Resolve paths
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Import the JS object (must be `export default {...}` in the JS file)
+const { default: data } = await import('./assets/iching_wilhelm_translation.js');
+
+// Write to JSON
+fs.writeFileSync('/Users/andrewbarker/developer/personal/api-sandbox/client/src/assets/hexagrams_full.json', JSON.stringify(data, null, 2), 'utf-8');
+console.log('✅ Converted to src/assets/hexagrams_full.json');
